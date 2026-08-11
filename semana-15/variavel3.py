6eec0e53847154fb41fb8020f31f1ddc38f83774
LimbertIvan220 <00001118176418sp@al.educacao.sp.gov.br>
2026-08-11 07:50:05 -0300

    envio das atividades de if e else

---
+++ semana-15/variavel3.py
@@ -0,0 +1,7 @@
+numero=int(input("Digite um numero: "))
+if numero > 0:
+    print(f"0 {numero} é positivo")
+elif numero < 0:
+    print(f"0 {numero} é positivo!")
+else:
+    print("O numero é zero")

464c6f1fa2e3bee132e327f030abe740d74d303e
LimbertIvan220 <00001118176418sp@al.educacao.sp.gov.br>
2026-09-15 10:12:14 -0300

    funcoes

---
+++ funcao_mutipli.py
@@ -0,0 +1,10 @@
+def mutiplicacao(numero1, numero2):
+    return numero1 * numero2
+
+num1 = 10
+num2 = 20
+n1 = 30
+n2 = 50
+resultado = mutiplicacao(num1, num2)
+print(f"{num1} * {num2} = {resultado}")
+print(f"{n1} * {n2} = {mutiplicacao(n1, n2)}")
